b12d02b3352a36776ca4b6b87e074d4d02bb5bea
inc <zeus522@gmail.com>
2024-04-15 18:04:12 +0200

    inc, init 001

---
+++ src/main/java/ch/dca/dcacore/DcaApplication.java
@@ -0,0 +1,13 @@
+package ch.dca.dcacore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/dca/dcacore/DcaApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.dca.dcacore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
