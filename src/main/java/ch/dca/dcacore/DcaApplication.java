f758d517401439c0a1a43974a3106f337b8693e0
inc <zeus522@gmail.com>
2024-04-22 15:24:00 +0200

    inc, cluster config 001

--- src/main/java/ch/dca/dcacore/DcaApplication.java
+++ src/main/java/ch/dca/dcacore/DcaApplication.java
@@ -3,11 +3,11 @@ package ch.dca.dcacore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DcaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DcaApplication.class, args);
 	}
-
 }
